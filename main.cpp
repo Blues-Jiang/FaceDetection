68b697fe20e13dbe4281f376d8c4ac473037b9ab
Blues_Fedora <victor1994y@qq.com>
2017-05-10 10:28:48 +0800

    add comshift

--- main.cpp
+++ main.cpp
@@ -8,6 +8,15 @@
 using namespace std;
 using namespace cv;
 
+//for CamShift
+bool backprojMode = false;
+bool selectObject = false;
+int trackObject = 0;
+bool showHist = true;
+Point origin;
+Rect selection;
+int vmin = 10, vmax = 256, smin = 30;
+
 void detectFace(Mat&, CascadeClassifier&, vector<Rect>&, double, bool);
 
 string cascadeName="./data/haarcascades/haarcascade_frontalface_alt.xml";
@@ -23,11 +32,11 @@ int main(){
   bool tryflip;
   */
   vector<Rect> faces;
-  Mat frame;
+  Mat frame,image;
   VideoCapture capture;
   const double scale=1.5;
 
-
+  //Face Detector
   //Load Haar Cascade
   CascadeClassifier cascade;
   try{
@@ -38,38 +47,43 @@ int main(){
   }
   //Open Camera
   capture.open(0);
-  if(capture.isOpened()){
-    cout << "Video capturing has been started ..." << endl;
-    //Set Camera
+  if(!capture.isOpened()){
+    cout << "***Could not initialize capturing...***"<<endl;
+    return -1;
+  }
 
-    capture.set(CAP_PROP_FRAME_WIDTH, 640);
-    capture.set(CAP_PROP_FRAME_HEIGHT, 480);
-    capture.set(CAP_PROP_FPS, 15);
+  cout << "Video capturing has been started ..." << endl;
+  namedWindow( "CamShift Demo", 0 );
+  //Set Camera
 
-    while(1){
-        capture >> frame;
-        if( frame.empty() ) break;
+  capture.set(CAP_PROP_FRAME_WIDTH, 640);
+  capture.set(CAP_PROP_FRAME_HEIGHT, 480);
+  capture.set(CAP_PROP_FPS, 20);
 
-        Mat frameTemp = frame.clone();
-        Mat mosaicTemp;
+  while(1){
+    capture >> frame;
+    if( frame.empty() ) break;
 
-        resize( frameTemp, mosaicTemp, Size(), 0.05, 0.05, INTER_AREA );
-        resize( mosaicTemp, mosaicTemp, Size(), 20, 20, INTER_NEAREST );
-        detectFace(frameTemp,cascade,faces,scale,0);
+    Mat image = frame.clone();
+    Mat mosaic;
 
-        //for ( size_t i = 0; i < faces.size(); i++ ){
-        while(!faces.empty()){
-          Rect ROI(cvRound(faces.back().x*scale), cvRound(faces.back().y*scale), cvRound(faces.back().width*scale), cvRound(faces.back().height*scale) );
-          mosaicTemp(ROI).copyTo(frameTemp(ROI));
-          //rectangle(frameTemp, cvPoint(cvRound(faces.back().x*scale),cvRound(faces.back().y*scale)), cvPoint(cvRound((faces.back().x+faces.back().width)*scale), cvRound((faces.back().y+faces.back().height)*scale)), Scalar(255,0,0), 3, 8, 0 );
-          faces.pop_back();
-        }
+    //Mosaic
+    resize( image, mosaic, Size(), 0.05, 0.05, INTER_AREA );
+    resize( mosaic, mosaic, Size(), 20, 20, INTER_NEAREST );
 
-        imshow( "Result", frameTemp );
-        //faces.clear();
-        char c = (char)waitKey(1);
-        if( c == 27 || c == 'q' || c == 'Q' ) break;
+    detectFace(image,cascade,faces,scale,0);
+
+    //for ( size_t i = 0; i < faces.size(); i++ ){
+    while(!faces.empty()){
+      Rect ROI(cvRound(faces.back().x*scale), cvRound(faces.back().y*scale), cvRound(faces.back().width*scale), cvRound(faces.back().height*scale) );
+      mosaic(ROI).copyTo(image(ROI));
+      //rectangle(frameTemp, cvPoint(cvRound(faces.back().x*scale),cvRound(faces.back().y*scale)), cvPoint(cvRound((faces.back().x+faces.back().width)*scale), cvRound((faces.back().y+faces.back().height)*scale)), Scalar(255,0,0), 3, 8, 0 );
+      faces.pop_back();
     }
+
+    imshow( "Face Detector", image );
+    char c = (char)waitKey(1);
+    if( c == 27 || c == 'q' || c == 'Q' ) break;
   }
 
   /*
@@ -81,6 +95,8 @@ int main(){
   waitKey();
   */
 }
+
+//void CamShift(){}
 
 void detectFace(Mat& image, CascadeClassifier& cascade,vector<Rect>& faces,double scale,bool tryflip){
   double timer = 0;
@@ -97,9 +113,9 @@ void detectFace(Mat& image, CascadeClassifier& cascade,vector<Rect>& faces,doubl
   //Face Detect
   timer = (double)getTickCount();
   cascade.detectMultiScale( smallImage, faces, 1.1, 2, 0
-      //|CASCADE_FIND_BIGGEST_OBJECT
-      //|CASCADE_DO_ROUGH_SEARCH
-      |CASCADE_SCALE_IMAGE,
+      |CASCADE_FIND_BIGGEST_OBJECT,
+      //|CASCADE_DO_ROUGH_SEARCH,
+      //|CASCADE_SCALE_IMAGE,
       Size(20, 20) );
   if( tryflip ){
       flip(smallImage, smallImage, 1);
@@ -107,7 +123,7 @@ void detectFace(Mat& image, CascadeClassifier& cascade,vector<Rect>& faces,doubl
                                 |CASCADE_SCALE_IMAGE,
                                 //|CASCADE_FIND_BIGGEST_OBJECT
                                 //|CASCADE_DO_ROUGH_SEARCH
-                                Size(30, 30) );
+                                Size(20, 20) );
       for( vector<Rect>::const_iterator r = facesflip.begin(); r != facesflip.end(); ++r ){
           faces.push_back(Rect(smallImage.cols - r->x - r->width, r->y, r->width, r->height));
       }
